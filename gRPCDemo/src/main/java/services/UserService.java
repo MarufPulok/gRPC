a09e303a532f83a16e8b7f131ba7875d44686db4
MarufPulok <53114706+MarufPulok@users.noreply.github.com>
2022-04-02 23:03:18 +0600

    Add files via upload

--- gRPCDemo/src/main/java/services/UserService.java
+++ gRPCDemo/src/main/java/services/UserService.java
@@ -15,24 +15,26 @@ public class UserService extends userGrpc.userImplBase {
         User.APIResponse.Builder response = User.APIResponse.newBuilder();
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/usersdb", "root", "15052000");
-
+            System.out.println("show");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/grpc-db","root", "15052000");
+            System.out.println("show2");
             String query = "INSERT INTO users(userName,password) "
                     + "VALUES(\"" + userName+"\",\""+password+"\")";
-
+            System.out.println("show3");
             Statement statement = connection.createStatement();
+            System.out.println("show4");
             statement.executeUpdate(query);
 
             response.setResponseCode(201).setResponseMessage("registration successful!");
 
-        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-            System.out.println("invalid");
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
 //            e.printStackTrace();
 
             response.setResponseCode(401).setResponseMessage("User already exists.");
+        }catch (Exception e) {
+//            e.printStackTrace();
+            System.out.println("invalid");
         }
 
         responseObserver.onNext(response.build());
@@ -44,7 +46,7 @@ public class UserService extends userGrpc.userImplBase {
     @Override
     public void login(User.LoginRequest request, StreamObserver<User.APIResponse> responseObserver) {
         System.out.println("Inside Login");
-        String url = "jdbc:mysql://localhost:3306/usersdb";
+        String url = "jdbc:mysql://localhost:3306/grpc-db";
         String user = "root";
         String pass = "15052000";
 
@@ -55,7 +57,7 @@ public class UserService extends userGrpc.userImplBase {
 
         User.APIResponse.Builder response = User.APIResponse.newBuilder();
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            //Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from users where userName = \""+userName+"\"");
@@ -76,9 +78,10 @@ public class UserService extends userGrpc.userImplBase {
             if (!b) {
                 response.setResponseCode(100).setResponseMessage("INVALID");
             }
-        } catch (ClassNotFoundException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
-        } catch (SQLException e) {
+        }catch (Exception e) {
             e.printStackTrace();
         }
 
